26ba8209c3d402a1b5f93685eadf8268b1a0a83f
Doug  Modie <dmodie@gmail.com>
2024-02-13 22:23:03 -0800

    example from tutorial working

---
+++ client-react-apollo/src/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//import ApolloClient from 'apollo-boost';
+////import { ApolloClient } from '@apollo/client';
+//import { ApolloProvider } from '@apollo/react-hooks';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetLocations {
+        users {
+          id
+          login
+          avatar_url
+        }
+      }
+    `,
+  })
+  .then((result) => console.log('res::', result));
+
+// client
+//   .query({
+//     query: gql`
+//       query GetLocations {
+//         locations {
+//           id
+//           name
+//           description
+//           photo
+//         }
+//       }
+//     `,
+//   })
+//   .then((result) => console.log('res::', result));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+serviceWorker.unregister();
